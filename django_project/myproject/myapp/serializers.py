ad576b76bdd7e1b4a3b0b298d307a87abb5d7a8e
Yaswanthi01 <53699150+Yaswanthi01@users.noreply.github.com>
2022-11-09 12:50:44 +0530

    changes

--- django_project/myproject/myapp/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-from myapp.models import Asset1
-
-class AssetSerializer(serializers.ModelSerializer):
-   class Meta:
-       model = Asset1
-       fields =  '__all__'
